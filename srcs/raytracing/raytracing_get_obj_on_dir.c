ae7ef8df57c6d011872767dd7e46309f1b8ad782
Arseniy MELIHOV <amelihov@e1r2p8.unit.ua>
2018-09-04 17:28:23 +0300

    delete trash

--- srcs/raytracing/raytracing_get_obj_on_dir.c
+++ srcs/raytracing/raytracing_get_obj_on_dir.c
@@ -6,7 +6,7 @@
 /*   By: amelihov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/20 20:39:35 by amelihov          #+#    #+#             */
-/*   Updated: 2018/08/23 14:25:34 by amelihov         ###   ########.fr       */
+/*   Updated: 2018/09/02 20:38:46 by amelihov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include "intersection.h"
 #include <stddef.h>
 
-t_object const	*raytracing_get_obj_on_dir(t_vect3d point1, t_vect3d point2,
+const t_object	*raytracing_get_obj_on_dir(t_vect3d point1, t_vect3d point2,
 				t_vect3d dir, const t_scene *scene)
 {
 	t_ray			ray;
@@ -25,7 +25,6 @@ t_object const	*raytracing_get_obj_on_dir(t_vect3d point1, t_vect3d point2,
 
 	ray.o = point1;
 	ray.d = dir;
-	(void)point2;
 	if (find_closest_intersection(scene, ray, &isect))
 	{
 		if (vect3d_is_fst_closer_snd_to(isect.dest, point2, point1))

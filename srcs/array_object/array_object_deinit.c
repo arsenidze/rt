8f01df8488816fd6cfeb3d72754f9b67fbbd54ee
Arseniy MELIHOV <amelihov@e1r7p14.unit.ua>
2018-09-17 17:42:21 +0300

    array_object_deinit.c normed

--- srcs/array_object/array_object_deinit.c
+++ srcs/array_object/array_object_deinit.c
@@ -6,11 +6,11 @@
 /*   By: amelihov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/17 16:59:23 by amelihov          #+#    #+#             */
-/*   Updated: 2018/09/17 17:07:13 by amelihov         ###   ########.fr       */
+/*   Updated: 2018/09/17 17:41:28 by amelihov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "array_object.h" 
+#include "array_object.h"
 #include <stdlib.h>
 
 void					array_object_deinit(t_array_object objects)
